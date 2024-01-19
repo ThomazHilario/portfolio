0950fe173189eb180fd2330a25af85f36694fcda
Thomaz Alves <thomazhilario5@gmail.com>
2024-01-19 12:23:16 -0300

    Adicionando animações de tela ao clicar no butão

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../Pages/Home'
 
+// Importando biblioteca de animacao
+import { gsap } from "gsap";
+import ScrollToPlugin from 'gsap/ScrollToPlugin';
 
 export default function RoutesPage(){
 
@@ -21,13 +24,40 @@ export default function RoutesPage(){
 
 
 function MenuDesktop(){
+    function scrollSection(text){
+        // Registrando plugin
+        gsap.registerPlugin(ScrollToPlugin)
+
+        if(window.innerWidth >= 1024){
+            // Caso o texto do button seja Home
+            if(text === 'Home'){ 
+                gsap.to(window,{duration:1,scrollTo:0})
+            }
+    
+            // Caso o texto do button seja Sobre
+            if(text === 'Sobre'){
+                gsap.to(window,{duration:1,scrollTo:200})
+            }
+    
+            // Caso o texto do button seja Projetos
+            if(text === 'Projetos'){
+                gsap.to(window,{duration:1,scrollTo:1086})
+            }
+    
+            // Caso o texto do button seja Contatos
+            if(text === 'Contatos'){
+                gsap.to(window,{duration:1,scrollTo:1508})    
+            }
+        }
+
+    }
     return(
         <div id='container_menu_desktop'>
             <menu id='menu_desktop'>
-                <button>Home</button>
-                <button>Sobre</button>
-                <button>Projetos</button>
-                <button>Contatos</button>
+                <button onClick={(e) => scrollSection(e.target.textContent)}>Home</button>
+                <button onClick={(e) => scrollSection(e.target.textContent)}>Sobre</button>
+                <button onClick={(e) => scrollSection(e.target.textContent)}>Projetos</button>
+                <button onClick={(e) => scrollSection(e.target.textContent)}>Contatos</button>
             </menu>
        </div>
     )
