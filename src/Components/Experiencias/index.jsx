d6d14fd334455bddfd819eb3562731dcac680ad9
Thomaz Alves <thomazhilario5@gmail.com>
2025-01-16 15:41:46 -0300

    Refactor: Update CV

---
+++ src/Components/Experiencias/index.jsx
@@ -0,0 +1,15 @@
+export const Experiencias = () => {
+    return(
+        <section>
+            {/* Titulo */}
+            <h1 className="titulo">Experiências</h1>
+
+            {/* Lista de experiências profissionais */}
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
+        </section>
+    )
+}
