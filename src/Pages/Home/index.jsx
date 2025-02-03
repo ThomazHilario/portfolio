fdab9fbcbcd8934b276585aff3d89c1a64bfef8b
Thomaz Alves <thomazhilario5@gmail.com>
2025-02-03 13:41:33 -0300

    refactor: Change css style for nesting css

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -27,21 +27,21 @@ export default function Home(){
                 <article id='container_description'>
 
                     {/* Container de introducao */}
-                    <div id='apresentacao'>
+                    <section id='apresentacao'>
                         <h1>Thomaz</h1>
                         <h1>Alves</h1>
 
                         <h3>Front-End Developer</h3>
 
-                    </div>
+                    </section>
 
-                    <div id='links_pessoais'>
+                    <section id='links_pessoais'>
 
                         <a href={curriculo} target='_blank' rel="noreferrer" id='curriculo' className='links_informacao'>Download CV</a>
 
 
                         <a href='https://api.whatsapp.com/send?phone=5585988274949' target="_blank" rel="noreferrer" className='links_informacao'>Entrar em Contato</a>
-                    </div>
+                    </section>
                 </article>
 
                 <div className='perfil'>
