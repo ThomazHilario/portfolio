3e8e4ec2bb95b52961398f2ed42c0cd25f22ff7c
Thomaz Alves <thomazhilario5@gmail.com>
2026-03-31 23:53:42 +0000

    fix: Add style in button in siderBar.

--- src/Components/Commons/Button/Button.tsx
+++ src/Components/Commons/Button/Button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = {
     primary: "bg-[#a3a6ff] h-9 w-auto max-w-40 rounded-lg px-2",
     secondary: "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded",
     danger: "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded",
-    sidebarItems:"bg-transparent text-neutral-300/90 min-w-48 hover:bg-[#494BD6]/10 justify-start border-l-3 border-transparent hover:border-violet-500",
+    sidebarItems:"bg-transparent text-neutral-300/90 min-w-48 hover:bg-[#494BD6]/10 justify-start border-l-3 border-transparent hover:border-violet-500 max-w-40 h-10 px-2 rounded-lg",
 }
 
 export const Button = ({ children, className, variant = undefined, type = "button", ...props }: ButtonProps) => {
