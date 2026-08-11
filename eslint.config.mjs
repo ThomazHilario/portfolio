d6c2b99c95c0cbedb9e303a6e9cb970e03463145
Thomaz Alves <thomazhilario5@gmail.com>
2026-08-11 13:36:19 +0000

    feat: add husky and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+
+const compat = new FlatCompat();
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +11,14 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+  {
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  },
 ]);
 
 export default eslintConfig;
